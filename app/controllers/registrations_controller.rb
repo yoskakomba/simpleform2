d1af9b9a67eb40c7bff7169d224191ad493700c6
Jo Kakomba <ariel.kakomba@gmail.com>
2016-01-08 06:04:14 +0000

    updating flash danger message on index form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < ApplicationController
       flash[:success] = "Your registration was created succesfully"
       redirect_to root_path
     else
-      flash[:error] = "Please complete the form, all required and can not be blank"
+      flash[:danger] = "Please complete the form, all required and can not be blank"
       redirect_to root_path
     end
   end
