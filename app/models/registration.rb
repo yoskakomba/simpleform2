9a1e490a233acd24615bfad39ad95cda35f16775
Jo Kakomba <ariel.kakomba@gmail.com>
2016-01-08 00:58:45 +0000

    update index form and flash messages partial

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,22 @@
 class Registration < ActiveRecord::Base
-  validates_length_of :name, :minimum => 5, :message => "blah blah blah"
-  
+  validates :name, :minimum => 5
+  validates :last_name, presence: true 
+  validates :date_of_birth, presence: true
+  validates :address, presence: true
+  validates :suburb, presence: true
+  validates :state, presence: true
+  validates :post_code, presence: true
+  validates :telephone, presence: true
+  validates :private_health_fund, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :reference, presence: true
+  validates :news_letter, presence: true
+  validates :occupation, presence: true
+  validates :body_conditions, presence: true
+  validates :past_medical_history, presence: true
+  validates :medication, presence: true
+  validates :symptoms, presence: true
+  validates :other_experience, presence: true
+  validates :preference, presence: true
+  validates :body_area, presence: true
 end 
